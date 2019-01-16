7f85b335ac2e620b18d136bbeb54a51739aa11f4
Hao Kung <HaoK@users.noreply.github.com>
2019-01-16 01:52:39 -0800

    Update DataProtectionProviderTests.cs

--- src/DataProtection/Extensions/test/DataProtectionProviderTests.cs
+++ src/DataProtection/Extensions/test/DataProtectionProviderTests.cs
@@ -162,8 +162,9 @@ namespace Microsoft.AspNetCore.DataProtection
             });
         }
 
-        [ConditionalFact]
+        [ConditionalFact] // https://github.com/aspnet/AspNetCore/issues/6720
         [X509StoreIsAvailable(StoreName.My, StoreLocation.CurrentUser)]
+        [OSSkipCondition(OperatingSystems.MacOSX)]        
         public void System_UsesProvidedCertificateNotFromStore()
         {
             using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
